1c67b75ff836dedc019cb5ae62d5445f214db53d
wnwlgns <24s718h0659@sonline20.sen.go.kr>
2025-05-20 20:31:38 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ interface Calculator {
         return a - b;
     }
 }
-public class SimpleCalculator implements Calculator {
+ class SimpleCalculator implements Calculator {
     @Override
     public int add(int a, int b) {
         return a + b;
