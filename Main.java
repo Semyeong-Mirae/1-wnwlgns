34bb7da0db7880249c6f0bd227beeb5ec610162f
wnwlgns <24s718h0659@sonline20.sen.go.kr>
2025-05-20 20:23:16 +0900

    Test wan

--- Main.java
+++ Main.java
@@ -1,23 +1,31 @@
 package calculator;
 
 import java.util.Scanner;
+interface Calculator {
+    int add(int a, int b);
+
+    default int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         SimpleCalculator calc = new SimpleCalculator();
 
-        // 사용자 입력 받기
-        System.out.print("첫 번째 숫자를 입력하세요: ");
         int a = scanner.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
+
         int b = scanner.nextInt();
 
-        // 연산 결과 출력
-        System.out.println("덧셈 결과: " + calc.add(a, b));
-        System.out.println("곱셈 결과: " + calc.multiply(a, b));
-        System.out.println("뺄셈 결과: " + Calculator.subtract(a, b));
+        System.out.println( calc.add(a, b));
+        System.out.println( calc.multiply(a, b));
+        System.out.println( Calculator.subtract(a, b));
 
         scanner.close();
     }
